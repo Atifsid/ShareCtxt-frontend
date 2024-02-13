e4310304ebfffdcd2308fb6eb39d888157f1572d
Atifsid <atifsiddiqui35@gmail.com>
2024-02-14 00:48:58 +0530

    fix build errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 "use client";
-import { redirect } from "next/navigation";
+import { redirect, useParams } from "next/navigation";
 import { useEffect } from "react";
 
-export default function Home({ id }: { id: string }) {
+export default function Home() {
+  const params = useParams<{ id: string }>()
+
   useEffect(() => {
-    if (id) {
-      redirect(`/${id}`)
+    if (params.id) {
+      redirect(`/${params.id}`)
     } else {
       redirect(`/edit`)
     }
-  }, [id]);
+  }, [params]);
+
+  return <p></p>;
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -38,10 +38,10 @@ const Header = ({ isEdit, initialTitle }: { isEdit: boolean; initialTitle?: stri
             <div className="relative w-1/2" data-te-input-wrapper-init>
                 <input
                     type="text"
-                    className="peer text-4xl font-bold tracking-widest block min-h-[auto] w-full rounded border-0 bg-transparent leading-[2.1] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                    className="peer text-4xl font-bold tracking-widest block min-h-[auto] w-full rounded border-0 bg-transparent leading-[2.1] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-20"
                     id="exampleFormControlInputHelper"
                     placeholder="Enter Title"
-                    value={title}
+                    value={title ? title : ''}
                     disabled={!isEdit}
                     onChange={(e) => setTitle(e.target.value)}
                 />
