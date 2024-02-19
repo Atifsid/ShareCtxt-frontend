ab6482aac9193cb3462d92741af1c8e0e72ef871
Atifsid <atifsiddiqui35@gmail.com>
2024-02-19 21:13:15 +0530

    misc.

--- src/core/constants/api-url-constant.ts
+++ src/core/constants/api-url-constant.ts
@@ -1,5 +1,5 @@
-export const BASE_URL = "https://backend.atifsiddiqui35.workers.dev/"
-export const UI_BASE_URL = "https://wong-rho.vercel.app"
+export const BASE_URL = "Your backend url"
+export const UI_BASE_URL = "Your Frontend url"
 
 export const ApiUrl = {
     getDocuments: 'doc',
