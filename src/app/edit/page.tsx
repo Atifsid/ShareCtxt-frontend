52c70a85c546df207fa8aeefe3bc120ef91d4380
Atifsid <atifsiddiqui35@gmail.com>
2024-02-19 13:18:33 +0530

    fix window is undefined

--- src/app/edit/page.tsx
+++ src/app/edit/page.tsx
@@ -7,21 +7,27 @@ import { useEffect, useState } from "react";
 const Edit = () => {
     const { setDoc } = useDocContext();
     const [content, setContent] = useState<string>();
+    const [hasWindow, setHasWindow] = useState<boolean>();
 
     useEffect(() => {
+        if (typeof window !== 'undefined') {
+            setHasWindow(true)
+        }
         content && setDoc!({ content: content });
     }, [content]);
 
-    let win;
+
 
     return (
         <main >
-            <div className={`${window.screen.availWidth <= 768 ? 'px-0' : 'px-24'} my-10`}><Header isEdit={true} /></div>
-            <div className={`flex flex-col items-center justify-between ${window.screen.availWidth <= 768 ? 'px-2' : 'px-24'}`}>
-                <div className="h-full w-full">
-                    <DisabledSSREditor onChange={(value: string) => setContent(value)} />
+            {hasWindow && <>
+                <div className={`${window.screen.availWidth <= 768 ? 'px-0' : 'px-24'} my-10`}><Header isEdit={true} /></div>
+                <div className={`flex flex-col items-center justify-between ${window.screen.availWidth <= 768 ? 'px-2' : 'px-24'}`}>
+                    <div className="h-full w-full">
+                        <DisabledSSREditor onChange={(value: string) => setContent(value)} />
+                    </div>
                 </div>
-            </div>
+            </>}
         </main>
     )
 }
